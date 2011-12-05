1e31bcb66bedd1409247ded1437cb97d111b84d2
Jairok <g.jairok@gmail.com>
2011-12-05 20:48:05 +0100

    First commit with RoR

---
+++ README.markdown
@@ -0,0 +1,16 @@
+uCapture-Backend
+================
+This project is the server side for the uCapture-Android-Frontend POC
+
+Specs
+-----
+* Upload images sent from an Android device using the uCapture frontend
+
+Requirements
+------------
+* Rails (3.1.0)
+* SQLite (current)
+
+WorkFlow
+--------
+* GIT Flow
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UCaptureBackend::Application.config.secret_token = '07e192a41cd6f99dc922ace461d7fc4e2d80371c14357a780a9f1ceb7d2de4522e89e1a1c36efff9fd2fd4f9757a0b187ecb8202c31a4953697df7492bac75f8'
